bc3312a1c05b272b1903b9c3f156d26430f9de4c
Davi Cardoso <122702586+davicardoso5524@users.noreply.github.com>
2024-09-16 22:02:52 -0300

    Add files via upload

--- estruturadedados-listacircular/src/App.java
+++ estruturadedados-listacircular/src/App.java
@@ -2,7 +2,19 @@
 public class App {
 
 	public static void main(String[] args) {
+		ListaCircular lista = new ListaCircular();
 		
+		lista.inserir(10);
+        lista.inserir(20);
+        lista.inserir(30);
+        lista.inserir(40);
+
+        System.out.println("Elementos da lista:");
+        lista.percorrer();  // Exibe: 10 20 30 40
+
+        lista.remover(30);
+        System.out.println("Após remover 30:");
+        lista.percorrer();  // Exibe: 10 20 40
 	}
 
 }
--- estruturadedados-listacircular/src/ListaCircular.java
+++ estruturadedados-listacircular/src/ListaCircular.java
@@ -20,7 +20,47 @@ public class ListaCircular {
 	
 	// Metódo Pra percorrer a lista ou Exibi-lá
 	public void percorrer() {
-		
+		if (cabeca == null) {
+			System.out.println("A lista está vazia");
+			return;
+		}
+		No atual = cabeca;
+		do {
+			System.out.print(atual.dado + " -> ");
+			atual = atual.proximo;
+		} while(atual != cabeca);
+		System.out.print("Aponta para cabeca");
+		System.out.println();
 	}
 	
+	// Metódo para remover um nó da lista
+	public void remover(int valor) {
+		if (cabeca == null) {
+			System.out.println("A lista está vazia");
+			return;
+		}
+		
+		No atual = cabeca;
+		No anterior = null;
+		
+		do {
+			if (atual.dado == valor) {
+				if (atual == cabeca) {
+					cabeca = cabeca.proximo;
+					cauda.proximo = cabeca;
+				} else if(atual == cauda) { 
+					System.out.println("entrou na condição que o valor é o msm da cauda");
+					cauda = anterior;
+					cauda.proximo = cabeca;
+				} else {
+					anterior.proximo = atual.proximo;
+				}
+				System.out.println("Elemento " + valor + " removido.");
+                return;
+			}
+			anterior = atual;
+			atual = atual.proximo;
+		}while (atual != cabeca);
+		System.out.println("Elemento " + valor + " não encontrado.");
+	}
 }
